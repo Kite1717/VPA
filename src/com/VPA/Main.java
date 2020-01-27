dd7e78d0290f84207415089e377e108b36e4b75a
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-01-27 18:37:20 +0300

    Fixed bug

--- src/com/VPA/Main.java
+++ src/com/VPA/Main.java
@@ -9,16 +9,17 @@ public class Main {
         int matrixSize ;
         while (true)
         {
-            System.out.println("Enter the square matrix of size");
+            System.out.println("Enter the square matrix size");
 
             try {
                 matrixSize = Integer.parseInt(sc.nextLine());
                 if(matrixSize >= 2)
                 break;
+                else System.out.println("Invalid matrix size");
             }
             catch (NumberFormatException ignored)
             {
-
+                System.out.println("Invalid matrix size");
             }
 
         }
