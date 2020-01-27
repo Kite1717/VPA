cfc96959d63b67f01616e5735036df5480c36f59
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-01-27 16:06:26 +0300

    fixed untracked files

--- src/com/VPA/Main.java
+++ src/com/VPA/Main.java
@@ -1,8 +1,28 @@
 package com.VPA;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-      VPA myVPA = new VPA(6);
+
+        int matrixSize ;
+        while (true)
+        {
+            System.out.println("Enter the square matrix of size");
+
+            try {
+                matrixSize = Integer.parseInt(sc.nextLine());
+                if(matrixSize >= 2)
+                break;
+            }
+            catch (NumberFormatException ignored)
+            {
+
+            }
+
+        }
+      VPA myVPA = new VPA(matrixSize);
       myVPA.setNeighborhoodMatrix();
       myVPA.getNeighborhoodMatrix();
       myVPA.applyVPA();
